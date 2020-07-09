587878b47ee57c531bdc305e269011f8c3b2ed6b
Enrico Caminada <enrico.caminada@gmail.com>
2020-07-09 13:53:49 +0200

    new file

---
+++ py_MSLearn_BranchCodeExecution.py
@@ -0,0 +1,15 @@
+print("Would you like to continue?")
+decition = str(input())
+
+if  decition == "No" or decition == "N":
+    print("Exiting")
+
+elif decition == "Yes" or decition == "Y":
+    print("Continuing ...")
+    print("Complete!")
+
+else:
+    print("Please try again and respond with yes or no.")
+    
+
+
